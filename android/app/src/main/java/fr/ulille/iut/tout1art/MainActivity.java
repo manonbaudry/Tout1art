5649f0b7d4f51d8703da67cf330dd6c72d0ce8c1
Sami LEJEUNE <sami.lejeune.etu@univ-lille.fr>
2019-03-20 13:42:17 +0100

    MAJ

--- android/app/src/main/java/fr/ulille/iut/tout1art/MainActivity.java
+++ android/app/src/main/java/fr/ulille/iut/tout1art/MainActivity.java
@@ -11,3 +11,46 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 }
+
+/*
+    candidature via formulaire
+    administrateur :
+    gestionnaire des différents artisants avec statut différent ; en validation , validé, refusé
+    gestion du compte de l'artisan : log avec mdp avec son statut
+    proposer des produits via le back office , ---> nom produit, descriptif , image
+    administrateur : gérer les demandes
+    gestion du catalogue produit avec statut , en attente , mise en ligne..
+    mise en ligne du produit
+    création du compte pour le client : formulaire nom prénom mail login mdp..
+    recherche par catégorie (v1) ranking produit (v2) géolocalisation (v2)
+    gestion de panier , consultation , retirer un article , modifier article (v2) , on peut créer
+    un panier sans s'identifier
+    valider son panier
+    choisir son adresse de livraison
+    paiement --> pas à faire
+    confirmation de la commande avec tout ce qui a été commandé.. où la livraison se fera , mail de
+    confirmation au client
+
+
+
+    ADMINISTRATEUR
+
+    backoffice sur toutes les commandes qui sont passées (en attente par l'artisan , validé , expédié)
+    gérer les réclamations (état : résolu..)
+    statistiques : nb de commande , CA réalisé par produit/artisan
+
+    ARTISAN
+
+    y'a une commande je peux changer délai , mettre à jour le statut
+    envoyer des informations à l'administrateur , l'avancé (v2)
+    une fois fabriqué, administrateur informé
+
+
+    CLIENT
+
+    Réclamation
+
+
+
+
+ */
