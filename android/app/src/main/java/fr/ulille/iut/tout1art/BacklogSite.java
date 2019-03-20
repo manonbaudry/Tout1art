ea16adbbace6bc27996277bd1dcbf2604828f912
Emeric Thebaud <emeric.thebaud@etudiant.univ-lille1.fr>
2019-03-20 14:30:33 +0100

    debut android

---
+++ android/app/src/main/java/fr/ulille/iut/tout1art/BacklogSite.java
@@ -0,0 +1,14 @@
+package fr.ulille.iut.tout1art;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class BacklogSite extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main); // modifier pour layout pour site
+    }
+}
