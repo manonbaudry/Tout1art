b0c542a3d71908f5d3dca2e56e929e652eb6e5ee
Sami LEJEUNE <sami.lejeune.etu@univ-lille.fr>
2019-03-21 14:36:51 +0100

    1ère communication avec le serveurgit add -Agit add -Agit add -A!

--- android/app/src/main/java/fr/ulille/iut/tout1art/testCo.java
+++ android/app/src/main/java/fr/ulille/iut/tout1art/testCo.java
@@ -4,29 +4,54 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import org.json.JSONObject;
+import org.json.JSONStringer;
+
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLConnection;
+import com.android.volley.Cache;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.Response.ErrorListener;
+import com.android.volley.Response.Listener;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.HttpHeaderParser;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 public class testCo extends AppCompatActivity {
+    private RequestQueue queue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test_co);
-        TextView t = findViewById(R.id.textView2);
-        String s = String.format("http://%s:%d%s", "10.0.2.2", 8080, "/api/v1/myresource");
-        t.setText(s);
-        try {
-            URL url = new URL("http://localhost:80/preouverture_java/liste.xml");
-            URLConnection urlCon = url.openConnection();
-            urlCon.setDoOutput(true);
-            urlCon.connect();
-            t.setText("reussi");
-        } catch (Exception e) {
-            t.setText("erreur");
-            //t.setText(e.getMessage());
-        }
+        queue = Volley.newRequestQueue(testCo.this);
+        String uri = "http://10.0.2.2:8080/api/v1/myresource";
+
+
+        StringRequest request = new StringRequest(
+                Request.Method.GET,
+                uri,
+                new Response.Listener<String>() {
+                    TextView tx = findViewById(R.id.textView2);
+                    @Override
+                    public void onResponse(String response) {
+                        tx.setText(response.toString());
+                        System.out.println("LA REPONSE C EST LA : "+response+ " FIN");
+                    }
+                },
+                new ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        System.out.println(error);
+                    }
+                });
+        queue.add(request);
     }
 
 }
