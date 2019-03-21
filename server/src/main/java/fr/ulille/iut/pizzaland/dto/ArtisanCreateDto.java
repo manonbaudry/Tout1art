adc3e25c71c1c105866bc2853c9721124094ac3b
Manon Baudry <man.baudry@gmail.com>
2019-03-21 09:43:59 +0100

    creation dto et ressource artisan

---
+++ server/src/main/java/fr/ulille/iut/pizzaland/dto/ArtisanCreateDto.java
@@ -0,0 +1,90 @@
+package fr.ulille.iut.pizzaland.dto;
+
+import fr.ulille.iut.pizzaland.dao.PizzaEntity;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Objects;
+
+public class ArtisanCreateDto {
+    
+    protected long id;
+    protected String nom;
+    protected String prenom; 
+    protected String adresse;
+    protected String mail;
+    protected String tel;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    /**
+	 * @return the prenom
+	 */
+	public String getPrenom() {
+		return prenom;
+	}
+
+	/**
+	 * @param prenom the prenom to set
+	 */
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+
+	/**
+	 * @return the adresse
+	 */
+	public String getAdresse() {
+		return adresse;
+	}
+
+	/**
+	 * @param adresse the adresse to set
+	 */
+	public void setAdresse(String adresse) {
+		this.adresse = adresse;
+	}
+
+	/**
+	 * @return the mail
+	 */
+	public String getMail() {
+		return mail;
+	}
+
+	/**
+	 * @param mail the mail to set
+	 */
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+
+	/**
+	 * @return the tel
+	 */
+	public String getTel() {
+		return tel;
+	}
+
+	/**
+	 * @param tel the tel to set
+	 */
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+    
+}
