89e2620baa640d0c7589e4f7127b1c763e216311
Emeric Thebaud <emeric.thebaud@etudiant.univ-lille1.fr>
2019-03-21 15:28:09 +0100

    suite connect

---
+++ android/app/src/main/java/fr/ulille/iut/tout1art/MonCompte.java
@@ -0,0 +1,26 @@
+package fr.ulille.iut.tout1art;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+public class MonCompte extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mon_compte);
+
+
+    }
+
+    /*
+    ida
+    login
+    nom
+    prenom
+    adresse
+    mail
+    tel
+     */
+}
