a2391d1d3b40b0b124c5032ef02ebd9f49118406
Sami LEJEUNE <sami.lejeune.etu@univ-lille.fr>
2019-03-22 16:43:10 +0100

    Mise a jour android

--- android/app/src/main/java/fr/ulille/iut/tout1art/FicheCommande.java
+++ android/app/src/main/java/fr/ulille/iut/tout1art/FicheCommande.java
@@ -3,6 +3,7 @@ package fr.ulille.iut.tout1art;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
@@ -14,14 +15,18 @@ import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class FicheCommande extends AppCompatActivity {
 
-    private TextView num_commande,prix,description,nom_produit;
+    private TextView num_commande,prix,description,nom_produit,delai;
     private int idArtisan;
     private RequestQueue queue;
     @Override
@@ -32,6 +37,7 @@ public class FicheCommande extends AppCompatActivity {
         this.prix = (TextView) findViewById(R.id.fiche_prix);
         this.description = (TextView) findViewById(R.id.fiche_description);
         this.nom_produit = (TextView) findViewById(R.id.fiche_nom_produit);
+        this.delai = (TextView) findViewById(R.id.fiche_delai);
         queue = Volley.newRequestQueue(FicheCommande.this);
         Intent in = getIntent();
         Bundle b = in.getExtras();
@@ -44,30 +50,20 @@ public class FicheCommande extends AppCompatActivity {
 
     }
 
-    public void showJsonArrayResponseCommande(JSONArray response) {
-        try {
-            for (int i = 0; i < response.length() ; i++) {
-                JSONObject obj = response.getJSONObject(i);
-                System.out.println("NOM : "+obj.getString("nom"));
-                if(obj.getInt("idArtisan") == idArtisan){
-
-                }
-            }
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
     public void showJsonArrayResponseProduit(JSONArray response) {
         try {
             for (int i = 0; i < response.length() ; i++) {
                 JSONObject obj = response.getJSONObject(i);
                 System.out.println("NOM : "+obj.getString("nom"));
                 if(obj.getInt("idArtisan") == idArtisan){
-                    if(obj.get("nom").equals(this.nom_produit.getText().toString())) {
-                        this.description.setText(obj.getString("description"));
-                        this.prix.setText(obj.getString("prix"));
+                    System.out.println(obj.getString("nom") + "<-- obj getString nom"  +  this.nom_produit.getText().toString());
+                    if(obj.getString("nom").equals(this.nom_produit.getText().toString())) {
+                        System.out.println("JE TRAITE " + obj.getString("nom"));
+                        this.description.setText(obj.getString("description") + ".");
+                        this.prix.setText(obj.getString("prix") + " €.");
+                        this.delai.setText(obj.getString("delai")+ " semaines.");
+                    } else {
+                        System.out.println("ICI LAG");
                     }
                 }
             }
@@ -76,21 +72,6 @@ public class FicheCommande extends AppCompatActivity {
         }
     }
 
-    public void getCommande() {
-        String uri = "http://10.0.2.2:8080/api/v1/produit";
-        JsonArrayRequest request = new JsonArrayRequest(Request.Method.GET,uri,null,new Response.Listener<JSONArray>() {
-            @Override
-            public void onResponse(JSONArray response) {
-                showJsonArrayResponseCommande(response);
-            }
-        }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                System.out.println(error);
-            }
-        });
-        queue.add(request);
-    }
 
     public void getProduit() {
         String uri = "http://10.0.2.2:8080/api/v1/produit";
@@ -108,14 +89,44 @@ public class FicheCommande extends AppCompatActivity {
         queue.add(request);
     }
 
-    public void refuser() {
-        String uri = "http://10.0.2.2:8080/api/v1/produit";
+    public void refuser(View view) {
+        String uri = "http://10.0.2.2:8080/api/v1/com/";
+        uri+="2?statut=ok";
+        StringRequest putRequest = new StringRequest(Request.Method.PUT, uri,
+                new Response.Listener<String>()
+                {
+                    @Override
+                    public void onResponse(String response) {
+                        // response
+                        Log.d("Response", response);
+                        System.out.println("REUSSITE");
+                    }
+                },
+                new Response.ErrorListener()
+                {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // error
+                       System.out.println(error);
+                    }
+                }
+        ) {
 
-        try {
+            @Override
+            protected Map<String, String> getParams()
+            {
+                Map<String, String>  params = new HashMap<String, String>();
+                params.put("statut", "Refuser");
 
-        } catch(Exception e) {
-            e.printStackTrace();
-        }
+                return params;
+            }
+
+        };
+        queue.add(putRequest);
+    }
+
+    public void accepter(View view){
+
     }
 
 
