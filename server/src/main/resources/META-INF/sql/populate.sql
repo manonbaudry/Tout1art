4ca03683c1ce6414298205470e815317cc7d9159
pereirani <nicolas.pereira.etu@univ-lille.fr>
2019-03-22 12:01:56 +0100

    ENFINNNN

--- server/src/main/resources/META-INF/sql/populate.sql
+++ server/src/main/resources/META-INF/sql/populate.sql
@@ -1,17 +1,17 @@
 DROP TABLE IF EXISTS artisan CASCADE;
 DROP TABLE IF EXISTS produit CASCADE;
 DROP TABLE IF EXISTS client CASCADE;
-DROP TABLE IF EXISTS commande CASCADE;
+DROP TABLE IF EXISTS com CASCADE;
 
 
 CREATE TABLE artisan  (id BIGINT IDENTITY NOT NULL,	login VARCHAR UNIQUE NOT NULL,	mdp VARCHAR NOT NULL,		  nom VARCHAR NOT NULL, prenom VARCHAR NOT NULL,		adresse VARCHAR,	mail VARCHAR,			tel VARCHAR(10),	PRIMARY KEY (id));
 CREATE TABLE produit  (id BIGINT IDENTITY NOT NULL,	nom VARCHAR NOT NULL,			description VARCHAR NOT NULL, srcImage VARCHAR,		prix DOUBLE,idArtisan INTEGER,  categorie VARCHAR,  sousCategorie VARCHAR,  commande INTEGER,	PRIMARY KEY (id));
 CREATE TABLE client   (id BIGINT IDENTITY NOT NULL,	login VARCHAR UNIQUE NOT NULL,	mdp VARCHAR NOT NULL,		  nom VARCHAR NOT NULL, prenom VARCHAR NOT NULL,		adresse VARCHAR,	mail VARCHAR,			tel VARCHAR(10),	PRIMARY KEY (id));
-CREATE TABLE commande (id BIGINT IDENTITY NOT NULL,	idProduit INTEGER, 				idClient INTEGER,			  statu VARCHAR,	  		PRIMARY KEY (id));
+CREATE TABLE com (id BIGINT IDENTITY NOT NULL,	idProduit INTEGER, 				idClient INTEGER,			  statu VARCHAR,	  		PRIMARY KEY (id));
 
 ALTER TABLE produit ADD FOREIGN KEY (idArtisan)  REFERENCES artisan(id);
-ALTER TABLE commande ADD FOREIGN KEY (idProduit)  REFERENCES produit(id);
-ALTER TABLE commande ADD FOREIGN KEY (idClient)  REFERENCES client(id);
+ALTER TABLE com ADD FOREIGN KEY (idProduit)  REFERENCES produit(id);
+ALTER TABLE com ADD FOREIGN KEY (idClient)  REFERENCES client(id);
 
 
 INSERT INTO artisan (login , mdp, nom, prenom, adresse, mail, tel) VALUES ('artisan1',	'123456789ABC',		'nom',		'prenom',	'adresse',		'mail@toto.fr',		'0320000000');
@@ -27,7 +27,7 @@ INSERT INTO produit (nom, description, srcImage, prix, idArtisan, categorie, sou
 INSERT INTO produit (nom, description, srcImage, prix, idArtisan, categorie, sousCategorie, commande) VALUES ('table base', 	 'Une table base',		'images/table.jpg',		'100',	 '1',	 'mobilier',	'table',	'1');
 INSERT INTO produit (nom, description, srcImage, prix, idArtisan, categorie, sousCategorie, commande) VALUES ('lampe de chevet', 'Une lampe de chevet', 'images/lampe.jpg',		'10.5',	 '1',	 'luminaire',	'lampe', 	'0');
 
-INSERT INTO commande (idProduit, idClient) VALUES ('5','1', '0');
+INSERT INTO com (idProduit, idClient, statu) VALUES ('5','1', 'En cours');
 
 /* 
 DROP TABLE IF EXISTS ingredientpizza CASCADE;
