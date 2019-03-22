35afd3b5f67a4721a92111a5858b0220a8895423
Emeric Thebaud <emeric.thebaud@etudiant.univ-lille1.fr>
2019-03-22 09:17:26 +0100

    test moncompte

--- android/app/src/main/java/fr/ulille/iut/tout1art/MonCompte.java
+++ android/app/src/main/java/fr/ulille/iut/tout1art/MonCompte.java
@@ -2,16 +2,82 @@ package fr.ulille.iut.tout1art;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 
 public class MonCompte extends AppCompatActivity {
+    int id;
+    private RequestQueue queue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mon_compte);
+        queue = Volley.newRequestQueue(MonCompte.this);
+        id = 2;
+        getArtisan();
 
+    }
 
+    public void getArtisan(){
+        String uri = "http://10.0.2.2:8080/api/v1/artisan";
+        JsonArrayRequest request = new JsonArrayRequest(
+                Request.Method.GET,
+                uri,
+                null,
+                new Response.Listener<JSONArray>() {
+                    TextView tx = findViewById(R.id.textView2);
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        showArt(response);
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        System.out.println(error);
+                    }
+                });
+        queue.add(request);
+    }
+
+    public void showArt(JSONArray response){
+        String str = "";
+        try {
+            for (int i = 0; i < response.length() ; i++) {
+                JSONObject obj = response.getJSONObject(i);
+                System.out.println("NOM : "+obj.getString("nom"));
+                if(obj.getInt("id") == id){
+                    str += obj.getInt("id" + "\n");
+                    str += obj.getString("nom" + "\n");
+                    str += obj.getString("prenom" + "\n");
+                    str += obj.getString("adresse" + "\n");
+                    str += obj.getString("mail" + "\n");
+                    str += obj.getString("tel" + "\n");
+
+                }
+            }
+
+            System.out.println("LE TXT "+str);
+            TextView text = findViewById(R.id.info);
+            text.setText(str);
+            text.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,LinearLayout.LayoutParams.WRAP_CONTENT));
+                //layout.addView(text);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     /*
