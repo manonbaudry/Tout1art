2f5ac7185a37dac95fee0b842b750ee6a3dadb23
Manon Baudry <man.baudry@gmail.com>
2019-03-25 14:45:49 +0100

    MAJ BDD + readme sprint 13 et 14

---
+++ doc/Sprint-14/Readme.md
@@ -0,0 +1,23 @@
+# Sprint 14
+
+## Travail effectué
+Fin de la correction des bugs sur le statut des commandes. 
+Fin de développement de la page de visualisation des commandes pour l'administrateur.
+MAJ de la charte graphique sous android
+
+
+## Résultats
+l'artisan peut refuser ou accepter une commande 
+l'artisan visulalise ses commandes en fonction de leurs statuts
+l'admin voit toutes les commandes et leurs statut
+Le visuel de l'application android correspond à l'architecture et à la charte graphique définie par l'étudiante en GEA et le porteur de projet.
+
+
+## Travail à faire au prochain sprint
+L'admin voit tous les produits, dont ceux ajoutés par un artisan (produits en attente de validation)
+modification du délai de fabrication des produits 
+
+
+## Ce que vous vous attendez à observer avec cette experience
+
+
--- server/src/main/resources/META-INF/sql/populate.sql
+++ server/src/main/resources/META-INF/sql/populate.sql
@@ -14,25 +14,25 @@ ALTER TABLE com ADD FOREIGN KEY (idProduit)  REFERENCES produit(id);
 ALTER TABLE com ADD FOREIGN KEY (idClient)  REFERENCES client(id);
 
 
-INSERT INTO artisan (login , mdp, nom, prenom, adresse, mail, tel) VALUES ('artisan1',	'azerty',		'nom',		'prenom',	'adresse',		'mail@toto.fr',		'0320000000');
-INSERT INTO artisan (login , mdp, nom, prenom, adresse, mail, tel) VALUES ('artisan2',	'123456789ABC',		'nom2',		'prenom2',	'adresse2',		'mail2@toto.fr',	'0320000001');
+INSERT INTO artisan (login , mdp, nom, prenom, adresse, mail, tel) VALUES ('artisan1',	'azerty',		'ART1',		'art1',	'adresse',		'mail@toto.fr',		'0320000000');
+INSERT INTO artisan (login , mdp, nom, prenom, adresse, mail, tel) VALUES ('artisan2',	'azerty',		'ART2',		'art2',	'adresse2',		'mail2@toto.fr',	'0320000001');
 INSERT INTO artisan (login , mdp, nom, prenom, adresse, mail, tel) VALUES ('admin',	'root',		'admin',		'Franck',	'adresse',		'mail@toto.fr',		'0320000000');
 
 INSERT INTO client (login , mdp, nom, prenom, adresse, mail, tel) VALUES ('client1',	'123456789ABC',		'nom',		'prenom',	'adresse',		'mail@toto.fr',		'0320000000');
 INSERT INTO client (login , mdp, nom, prenom, adresse, mail, tel) VALUES ('client2',	'123456789ABC',		'nom2',		'prenom2',	'adresse2',		'mail2@toto.fr',	'0320000001');
 
 
-INSERT INTO produit (nom, description, srcImage, prix, idArtisan, categorie, sousCategorie, commande, delai, statut) VALUES ('miroir',	 'Un miroir moderne',	'images/miroir.png',	'10.5',	 '1',	 'deco',	'miroir',	'0','3', 'en vente');
+INSERT INTO produit (nom, description, srcImage, prix, idArtisan, categorie, sousCategorie, commande, delai, statut) VALUES ('miroir',	 'Un miroir moderne',	'images/miroir.png',	'10.5',	 '2',	 'deco',	'miroir',	'0','3', 'en vente');
 INSERT INTO produit (nom, description, srcImage, prix, idArtisan, categorie, sousCategorie, commande, delai, statut) VALUES ('lampe chêne',	 'Inspiration nature pour cette lampe à poser en bois massif.  La technique du chantournage utilisée sur le pied confère à l’objet un look design végétal.',	'images/lampeChene.jpg',	'10.5',	 '1',	 'luminaire',	'lampe',	'0','3', 'en vente');
 INSERT INTO produit (nom, description, srcImage, prix, idArtisan, categorie, sousCategorie, commande, delai, statut) VALUES ('lampe ampoule',	 'Cette lampe en forme d’ampoule qui joue avec la lumière est en frêne massif, protégé d’un vernis mat incolore 2 couches. Les 8 lamelles de bois qui la composent se déplient à l’envie.',	'images/lampeAmpoule.jpg',	'15.5',	 '1',	 'luminaire',	'lampe',	'0','3', 'en vente');
 INSERT INTO produit (nom, description, srcImage, prix, idArtisan, categorie, sousCategorie, commande, delai, statut) VALUES ('chaise simple',	 'Une chaise simple',	'images/chaise2.png',		'10.5',	 '1',	 'mobilier',	'chaise',	'0', '1', 'en vente');
-INSERT INTO produit (nom, description, srcImage, prix, idArtisan, categorie, sousCategorie, commande, delai, statut) VALUES ('chaise moderne',	 'Une chaise moderne',	'images/chaise.png',		'150',	 '1',	 'mobilier',	'chaise',	'0', '1', 'en vente');
+INSERT INTO produit (nom, description, srcImage, prix, idArtisan, categorie, sousCategorie, commande, delai, statut) VALUES ('chaise moderne',	 'Une chaise moderne',	'images/chaise.png',		'150',	 '2',	 'mobilier',	'chaise',	'0', '1', 'en vente');
 INSERT INTO produit (nom, description, srcImage, prix, idArtisan, categorie, sousCategorie, commande, delai, statut) VALUES ('lampe bulles',	 'Lampe bulles réalisée en Freine olivier recouvert d’une double couche de vernis acrylique. Technique de chantournage.',	'images/lampeBulles.png',		'120',	 '1',	 'luminaire',	'lampe',	'0', '1','en vente');
 INSERT INTO produit (nom, description, srcImage, prix, idArtisan, categorie, sousCategorie, commande, delai, statut) VALUES ('lampe',	 'Une chaise simple',	'images/chaise.jpg',	'10.5',	 '1',	 'mobilier',	'chaise',	'0','3','en vente');
 INSERT INTO produit (nom, description, srcImage, prix, idArtisan, categorie, sousCategorie, commande, delai, statut) VALUES ('chaise noire',	 'Une chaise simple',	'images/chaiseNoire.jpg',	'10.5',	 '1',	 'mobilier',	'chaise',	'0','3','en vente');
 INSERT INTO produit (nom, description, srcImage, prix, idArtisan, categorie, sousCategorie, commande, delai, statut) VALUES ('chaise blanche',	 'Une chaise simple',	'images/chaiseBlanche.jpg',	'10.5',	 '1',	 'mobilier',	'chaise',	'0','3','en vente');
 INSERT INTO produit (nom, description, srcImage, prix, idArtisan, categorie, sousCategorie, commande, delai, statut) VALUES ('vase simple',	 'Un vase simple',		'images/vase.jpg',	 	'3.5',	 '1',	 'deco',	 	'vase',	 	'0', '5','en vente');
-INSERT INTO produit (nom, description, srcImage, prix, idArtisan, categorie, sousCategorie, commande, delai, statut) VALUES ('lampe simple',	 'Une lampe simple',	'images/lampe.jpg', 	'5.5',	 '1',	 'luminaire',	'lampe',	'0', '7','en vente');
+INSERT INTO produit (nom, description, srcImage, prix, idArtisan, categorie, sousCategorie, commande, delai, statut) VALUES ('lampe simple',	 'Une lampe simple',	'images/lampe.jpg', 	'5.5',	 '2',	 'luminaire',	'lampe',	'0', '7','en vente');
 INSERT INTO produit (nom, description, srcImage, prix, idArtisan, categorie, sousCategorie, commande, delai,statut) VALUES ('table basse', 	 'Une table basse 1',		'images/tableBasseSimple.jpg',		'100',	 '1',	 'mobilier',	'table',	'1','4','en vente');
 INSERT INTO produit (nom, description, srcImage, prix, idArtisan, categorie, sousCategorie, commande, delai, statut) VALUES ('table basse', 	 'Une table basse 2',		'images/tableBasseComplexe.jpg',		'100',	 '1',	 'mobilier',	'table',	'1','4','en vente');
 
